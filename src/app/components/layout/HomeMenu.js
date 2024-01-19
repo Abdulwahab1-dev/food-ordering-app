969cc9f08fa11f1e613b7e4608e861a9f2e5a7a3
Abdulwahab1-dev <wahab.proclamationhub@gmail.com>
2024-01-19 17:03:52 +0500

    website deploy in vercel

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -13,45 +13,6 @@ const HomeMenu = () => {
         </h2>
       </div>
       <div className="grid gap-4 md:grid-cols-3 md:gap-4 py-[2rem]">
-        {/* <div className="bg-gray-300 p-4 rounded-md flex flex-col items-center justify-center py-[2rem]">
-          <img src="/pizza.png" alt="Pizza" width={300} />
-          <h1 className="font-semibold my-2 text-xl md:text-2xl">
-            Pepperoni Pizza
-          </h1>
-          <p className="text-gray-600 text-sm md:text-xl my-[1.5rem] text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-          <button className="bg-primary text-white px-[2rem] py-[0.5rem] rounded-full font-semibold text-[1rem] md:py-[0.5rem] md:text-[1.3rem] ">
-            Add To cart $12
-          </button>
-        </div> */}
-        {/* <div className="bg-gray-300 p-4 rounded-md flex flex-col items-center justify-center py-[2rem]">
-          <img src="/pizza.png" alt="Pizza" width={300} />
-          <h1 className="font-semibold my-2 text-xl md:text-2xl">
-            Pepperoni Pizza
-          </h1>
-          <p className="text-gray-600 text-sm md:text-xl my-[1.5rem] text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-          <button className="bg-primary text-white px-[2rem] py-[0.5rem] rounded-full font-semibold text-[1rem] md:py-[0.5rem] md:text-[1.3rem] ">
-            Add To cart $12
-          </button>
-        </div> */}
-        {/* <div className="bg-gray-300 p-4 rounded-md flex flex-col items-center justify-center py-[2rem]">
-          <img src="/pizza.png" alt="Pizza" width={300} />
-          <h1 className="font-semibold my-2 text-xl md:text-2xl">
-            Pepperoni Pizza
-          </h1>
-          <p className="text-gray-600 text-sm md:text-xl my-[1.5rem] text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-          <button className="bg-primary text-white px-[2rem] py-[0.5rem] rounded-full font-semibold text-[1rem] md:py-[0.5rem] md:text-[1.3rem] ">
-            Add To cart $12
-          </button>
-        </div> */}
         <MenuItem/>
         <MenuItem/>
         <MenuItem/>
