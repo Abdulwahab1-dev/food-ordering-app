a86c6916ff14edfdf7508bea1d1033cc1f7ecc8d
Abdulwahab1-dev <wahab.proclamationhub@gmail.com>
2024-01-25 01:10:58 +0500

    remove quotes

--- src/app/components/about/About.js
+++ src/app/components/about/About.js
@@ -9,18 +9,18 @@ const About = () => {
         <div className="max-w-6xl mx-auto my-[1.4rem] px-[2rem] md:px-[2rem] flex flex-col gap-4">
         <p className="text-gray-600 font-semibold  text-[1rem] text-justify md:text-center md:text-[1.2rem]" >
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry quote: &apos;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          including versions of Lorem Ipsum
         </p>
         <p className="text-gray-600 font-semibold  text-[1rem] text-justify md:text-center md:text-[1.2rem]" >
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry quote: &apos;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
